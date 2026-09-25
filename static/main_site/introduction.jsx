2257bd0bdf5e85636134759d04529d51ae0135c1
zhenghao <zhenghao@calicolabs.com>
2026-09-25 14:53:45 -0700

    New introduction

--- static/main_site/introduction.jsx
+++ static/main_site/introduction.jsx
@@ -2,30 +2,21 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 
 export var introduction = <div>
-    <p>I am a machine learning researcher who is passionate about applying AI/ML to develop novel therapeutics that extend human healthspan.</p>
+<p>I am a machine learning researcher who is passionate about applying AI/ML to develop novel therapeutics for patients.</p>
 
-   <p>I'm currently director of ML at <a href="https://www.calicolabs.com">Calico Life Sciences</a> — a biotech company that studies aging to
-    enable people to live longer and healthier lives.</p>
+<p>I'm currently VP of AI &amp; ML at <a href="https://insitro.com/">insitro</a> — a biotech company combining machine learning with human-centric data to reimagine drug discovery and development. I lead the ML strategy behind our platform: work that runs from discovering novel biology in the genetics of human cohorts and perturbation screens of cellular models, through the precision engineering of therapeutic molecules, to bringing new medicines to patients.</p>
 
-   <p>At Calico, I help lead a team of AI / ML scientists and engineers, working on projects that accelerate the drug development pipeline spanning
-    basic research, target identification / validation through to lead gen / opt.</p>
+<p>My research interests have included:</p>
 
-   <p>My current research interests are:
+<ul>
+  <li>Predicting how cells and people respond to genetic or chemical perturbations by applying self-supervised representation learning to high-content imaging / cellular morphology</li>
+  <li>Understanding how the immune microenvironment is perturbed with aging and in cancer using graph-based models over spatial transcriptomics / proteomics and single-cell sequencing data</li>
+  <li>Enabling rapid development of therapeutic molecules and optimization for drug-like properties across therapeutic modalities using co-folding and generative models</li>
+  <li>Quantifying dimensions of age-related decline and disease using multi-omics / high-dimensional time series data from longitudinal studies (e.g., medical imaging, metabolic markers, blood biomarkers, and wearables) using probabilistic graphical models</li>
+</ul>
 
-    <ul>
-       <li>Predicting how cells will respond to genetic or chemical perturbations by applying self-supervised representation learning to high-content imaging / cellular morphology</li>
-       <li>Understanding how the immune microenvironment is perturbed with aging and in cancer using graph-based models to model spatial transcriptomics / proteomics and single-cell sequencing data</li>
-       <li>Enabling rapid development of therapeutic molecules and optimization for drug-like properties in various therapeutic modalities using co-folding and generative models</li>
-       <li>Quantifying dimensions of age-related decline using multi-omics / high-dimensional time series data from longitudinal studies (e.g., medical imaging, metabolic markers, blood biomarkers, and wearables)
-       using probabilistic graphical models</li>
-    </ul>
-    </p>
+<p>Before insitro, I was Director of ML at <a href="https://www.calicolabs.com/">Calico Life Sciences</a> — a biotech company that studies aging to enable people to live longer and healthier lives. At Calico I helped lead a team of AI / ML scientists and engineers, working on projects that accelerate the drug development pipeline spanning basic research, target identification / validation through to lead gen / opt. Before Calico, I was a data scientist and engineer at <a href="https://www.coursera.org/">Coursera</a>. I got my B.S. and M.S. (in Computer Science) at Stanford where I worked with <a href="http://www.andrewng.org/">Andrew Ng</a> and <a href="http://ai.stanford.edu/users/koller/">Daphne Koller</a> on deep learning and computational biology.</p>
 
-    <p>Before Calico, I was a data scientist and engineer at <a href="https://www.coursera.org">Coursera</a> for 5 years.
-    I got my B.S. and M.S. (in Computer Science) at Stanford where I worked
-    with <a href="http://www.andrewng.org/">Andrew Ng</a> and <a href="http://ai.stanford.edu/users/koller/">Daphne Koller</a> on deep learning and computational biology.</p>
-    <p>I enjoy taking photographs (especially natural landscapes) and you can check out some of my photos <a href="slideshow.html">here</a>.
-    I also helped start and currently work with <a href="https://pdquery.org">PDQuery</a> a non-profit that connects
-    STEM experts with public defenders who need their expertise.</p>
-  </div>
+<p>I enjoy taking photographs (especially natural landscapes) and you can check out some of my photos <a href="https://z-chen.net/slideshow.html">here</a>.</p>
+</div>
 
