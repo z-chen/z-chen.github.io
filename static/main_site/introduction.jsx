fe655825b39e3b2ec107ce4e8e8c3708d5b3eb52
zhenghao <zhenghao@calicolabs.com>
2026-09-25 15:10:54 -0700

    Fix bad intro

--- static/main_site/introduction.jsx
+++ static/main_site/introduction.jsx
@@ -4,13 +4,13 @@ var ReactDOM = require('react-dom');
 export var introduction = <div>
 <p>I am a machine learning researcher who is passionate about applying AI/ML to develop novel therapeutics for patients.</p>
 
-<p>I'm currently VP of AI &amp; ML at <a href="https://insitro.com/">insitro</a> — a biotech company combining machine learning with human-centric data to reimagine drug discovery and development. I lead the ML strategy behind our platform: work that runs from discovering novel biology in the genetics of human cohorts and perturbation screens of cellular models, through the precision engineering of therapeutic molecules, to bringing new medicines to patients.</p>
+<p>I'm currently VP of AI &amp; ML at <a href="https://insitro.com/">insitro</a> — a biotech company generating, integrating, and scaling massive data sets to build an integrated model of causal human biology. At insitro, I lead the ML strategy behind our platform: work that spans discovering novel biology from genetics of human cohorts and perturbation screens of cellular models, through the precision engineering of therapeutic molecules, to bringing new medicines to patients.</p>
 
 <p>My research interests have included:</p>
 
 <ul>
-  <li>Predicting how cells and people respond to genetic or chemical perturbations by applying self-supervised representation learning to high-content imaging / cellular morphology</li>
-  <li>Understanding how the immune microenvironment is perturbed with aging and in cancer using graph-based models over spatial transcriptomics / proteomics and single-cell sequencing data</li>
+  <li>Predicting how cells respond to genetic or chemical perturbations by applying representation learning to high-content imaging / cellular morphology</li>
+  <li>Understanding how the immune microenvironment is perturbed with aging and in cancer using graph-based models on spatial transcriptomics / proteomics and single-cell sequencing data</li>
   <li>Enabling rapid development of therapeutic molecules and optimization for drug-like properties across therapeutic modalities using co-folding and generative models</li>
   <li>Quantifying dimensions of age-related decline and disease using multi-omics / high-dimensional time series data from longitudinal studies (e.g., medical imaging, metabolic markers, blood biomarkers, and wearables) using probabilistic graphical models</li>
 </ul>
